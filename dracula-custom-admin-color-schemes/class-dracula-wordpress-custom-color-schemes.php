b20e3941e1c16cd93ee4584e53683c5c40cbfb5e
rdelbem <rodrigodelbem@gmail.com>
2023-06-23 02:00:15 -0300

    test: commit

--- dracula-custom-admin-color-schemes/class-dracula-wordpress-custom-color-schemes.php
+++ dracula-custom-admin-color-schemes/class-dracula-wordpress-custom-color-schemes.php
@@ -10,7 +10,8 @@
  * Class that adds the styles to the admin area
  * it also does some basic checks
  */
-class Dracula_WordPress_Custom_Color_Schemes {
+class Dracula_WordPress_Custom_Color_Schemes
+{
 
 
 
@@ -32,26 +33,28 @@ class Dracula_WordPress_Custom_Color_Schemes {
 	/**
 	 * Constructor function that calls add actions
 	 */
-	public function __construct() {
-		add_action( 'admin_enqueue_scripts', array( $this, 'load_default_css' ) );
+	public function __construct()
+	{
+		add_action('admin_enqueue_scripts', array($this, 'load_default_css'));
 
-		add_action( 'admin_init', array( $this, 'add_colors' ) );
+		add_action('admin_init', array($this, 'add_colors'));
 	}
 
 	/**
 	 * Register color schemes.
 	 */
-	public function add_colors() {
+	public function add_colors()
+	{
 		$suffix = is_rtl() ? '-rtl' : '';
 
 		wp_admin_css_color(
 			'smackdown',
-			__( 'Dracula', 'admin_schemes' ),
-			plugins_url( 'smackdown/colors' . $suffix . '.css', __FILE__ ),
-			array( '#282a36', '#44475a', '#8be9fd', '#f8f8f2' ),
+			__('Dracula', 'admin_schemes'),
+			plugins_url('smackdown/colors' . $suffix . '.css', __FILE__),
+			array('#282a36', '#44475a', '#8be9fd', '#f8f8f2'),
 			array(
-				'base'    => '#3299bb',
-				'focus'   => '#452b72',
+				'base' => '#3299bb',
+				'focus' => '#452b72',
 				'current' => '#f5f5f5',
 			)
 		);
@@ -64,13 +67,14 @@ class Dracula_WordPress_Custom_Color_Schemes {
 	 * @import it from a plugin stylesheet. Also force-load the default colors
 	 * on the profile screens, so the JS preview isn't broken-looking.
 	 */
-	public function load_default_css() {
+	public function load_default_css()
+	{
 		global $wp_styles;
 
-		$color_scheme = get_user_option( 'admin_color' );
+		$color_scheme = get_user_option('admin_color');
 
-		$scheme_screens = apply_filters( 'acs_picker_allowed_pages', array( 'profile', 'profile-network' ) );
-		if ( in_array( $color_scheme, $this->colors, true ) || in_array( get_current_screen()->base, $scheme_screens, true ) ) {
+		$scheme_screens = apply_filters('acs_picker_allowed_pages', array('profile', 'profile-network'));
+		if (in_array($color_scheme, $this->colors, true) || in_array(get_current_screen()->base, $scheme_screens, true)) {
 			$wp_styles->registered['colors']->deps[] = 'colors-fresh';
 		}
 
